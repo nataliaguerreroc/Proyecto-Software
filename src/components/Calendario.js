a4f0fbf0b888e971a5d4b0c87759105aa1eb49c3
JuanPabloGiraldoSoftware <juanpgym@gmail.com>
2021-11-29 01:32:35 -0500

    added simulated login and calendar view

--- src/components/Calendario.js
+++ src/components/Calendario.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
+import imag1 from "../images/act2.jpg";
+import './Calendario.css';
+import FullCalendar from '@fullcalendar/react' 
+import dayGridPlugin from '@fullcalendar/daygrid' 
 
 export default function Calendario(){
-    return(
-        <>
-        <h1 className = 'calendario'> Calendario </h1>
-        </>
-    );
+  return(
+    <div className = "acercade_cont">
+    
+    <div className = "contenedor-imagenac">
+      <img src={imag1} className="img-fluid imagen_acerca" alt="imag-pensionados"  width = "100%"></img>
+    </div>
+
+          <h1 className = "title"> Calendario </h1>
+          <div className = "text"> 
+          <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            events={[
+              { title: 'Festival Brújula al Sur', date: '2021-11-30' },
+              { title: 'Festival Brújula al Sur', date: '2021-12-01' },
+              { title: 'Festival Brújula al Sur', date: '2021-12-02' },
+              { title: 'Festival Brújula al Sur', date: '2021-12-03' },
+              { title: 'Festival Brújula al Sur', date: '2021-12-04' },
+              { title: 'The Mills', date: '2021-12-04' },
+              { title: 'Actividad 4', date: '2021-12-25' },
+              { title: 'Actividad 4', date: '2021-12-26' },
+              { title: 'Actividad 4', date: '2021-12-27' },
+              { title: 'Actividad 4', date: '2021-12-28' },
+              { title: 'Actividad 4', date: '2021-12-29' },
+              { title: 'Actividad 4', date: '2021-12-30' },
+              { title: 'POP FESTIVAL', date: '2021-12-15' }
+            ]}
+          />
+      </div>
+
+    </div>
+);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,39 @@
 import React, { useState } from 'react'
-import { Button } from './Button'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import Dropdown from './Dropdown'
+import { Modal, TextField, Button as MButton } from '@material-ui/core';
+import { makeStyles} from '@material-ui/core/styles';
 
 function Navbar() {
+
+    const useStyles = makeStyles((theme)=>({
+        modal: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: 'white',
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2,4,3),
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+        },
+        textField:{
+            width:'100%',
+        },
+        button:{
+            color: "white",
+            borderRadius: "100px",
+            marginLeft: "1%"
+    
+            }
+    }))
+
+    const styles = useStyles();
+
+    const [currentModal, setModal] = useState('none');
+
     const [click, setClick] = useState(false);
 
     const [dropdown, setDropdown] = useState(false);
@@ -12,6 +41,36 @@ function Navbar() {
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+    const openModalLogin= ()=>{
+        setModal('Login')
+    }
+
+    const login = ()=>{
+        document.getElementById("loginB").style.visibility = "hidden"
+        document.getElementById("logged").style.visibility = "visible"
+        closeAnyModal()
+    }
+
+    const closeAnyModal=()=>{
+        setModal('none')
+    }
+
+    const modalLogin = (
+           
+        <div className={styles.modal}>
+        <div align='center'>
+            <h2>Iniciar sesión</h2>
+        </div>
+        <TextField id="userFieldL" label="Usuario" className={styles.textField} />
+        <br/>
+        <TextField id="passwordFieldL" label="Contraseña" className={styles.textField} type="password" />
+        <br/> <br/>
+        <div align='right'>
+        <MButton color="primary" onClick={login}>Autenticar</MButton>
+        <MButton onClick={()=>closeAnyModal()}>Cancelar</MButton>
+        </div>
+     </div>)
 
     const onMouseEnter = () => {
         if(window.innerWidth < 960) {
@@ -31,8 +90,14 @@ function Navbar() {
 
 
     return (
-        <>
+        <>     
         <nav className="navbar">
+        <Modal
+        open={currentModal==='Login'}
+        onClose={openModalLogin}>
+        {modalLogin}
+        </Modal>
+
                 <Link to = "/" className = "navbar-logo">
                     VIDA FELIZ <i className="far fa-grin-beam"></i>
                 </Link>
@@ -72,14 +137,19 @@ function Navbar() {
                         </Link>
                     </li>
 
-                    <li className = 'nav-item'>
-                        <Link to = '/ingresar' className = 'nav-links-mobile' onClick = {closeMobileMenu}>
-                            Ingresar
-                        </Link>
+                    <li id = 'userCurrent' className = 'nav-item'>
+                    <h1  style = {{visibility:'hidden'}} id="logged" className = 'nav-links'>
+                           Bienvenido, TestUser
+                    </h1>
+                    </li>
+
+                    <li id = 'loginB' className = 'nav-item'>
+                        <MButton style = {{color: "white", fontFamily: 'Dosis, sans-serif'}} className = 'nav-links-mobile' onClick = {openModalLogin}>
+                            Iniciar Sesión
+                        </MButton>
                     </li>
 
                 </ul>
-                <Button />
 
         </nav>
             
